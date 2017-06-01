f16f2c61e7e659ea06e85d4f5b6199c76f1200bb
Joseph Matthias Goh <govtech@joeir.net>
2017-06-01 16:18:23 +0800

    adding code for travis ci to provide test key

--- test/test.helper.js
+++ test/test.helper.js
@@ -9,19 +9,23 @@ process.stdout.write('DONE.\n\n');
 
 process.stdout.write('[helper] retrieving consumer key from ./.env file...');
 try {
-  const env = fs.readFileSync(path.join(process.cwd(), './.env')).toString();
   const consumerKeyIdentifier = 'DATAGOVSG_CONSUMER_KEY=';
-  const indexOfConsumerKeyIdentifier = env.indexOf(consumerKeyIdentifier);
-  if(indexOfConsumerKeyIdentifier === -1) {
-    throw new Error(`"${consumerKeyIdentifier}" key was not found in ./.env.`);
+  if(process.env.DATAGOVSG_CONSUMER_KEY) {
+    global.DATAGOVSG_CONSUMER_KEY = process.env.DATAGOVSG_CONSUMER_KEY;
+  } else {
+    const env = fs.readFileSync(path.join(process.cwd(), './.env')).toString();
+    const indexOfConsumerKeyIdentifier = env.indexOf(consumerKeyIdentifier);
+    if(indexOfConsumerKeyIdentifier === -1) {
+      throw new Error(`"${consumerKeyIdentifier}" key was not found in ./.env.`);
+    }
+    const startOfConsumerKey = env.substr(
+      indexOfConsumerKeyIdentifier + consumerKeyIdentifier.length
+    );
+    const indexOfNextNewline = startOfConsumerKey.indexOf('\n');
+    global.DATAGOVSG_CONSUMER_KEY = (indexOfNextNewline !== -1) ?
+      startOfConsumerKey.substr(0, indexOfNextNewline) :
+      startOfConsumerKey;
   }
-  const startOfConsumerKey = env.substr(
-    indexOfConsumerKeyIdentifier + consumerKeyIdentifier.length
-  );
-  const indexOfNextNewline = startOfConsumerKey.indexOf('\n');
-  global.DATAGOVSG_CONSUMER_KEY = (indexOfNextNewline !== -1) ?
-    startOfConsumerKey.substr(0, indexOfNextNewline) :
-    startOfConsumerKey;
   process.stdout.write('DONE.\n\n');
 } catch(ex) {
   process.stdout.write('\n[helper] ERROR: ./.env not found OR did not contain "DATAGOVSG_CONSUMER_KEY" key.\n');
